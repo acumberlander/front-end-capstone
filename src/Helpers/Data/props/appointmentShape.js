078b3ec6a4c386672081f85743796418f8cdbd81
Austin Cumberlander <austincumberlander@gmail.com>
2019-01-23 20:29:23 -0600

    add is not working right now. wip.

--- src/Helpers/Data/props/appointmentShape.js
+++ src/Helpers/Data/props/appointmentShape.js
@@ -2,11 +2,12 @@ import PropTypes from 'prop-types';
 
 const appointmentShape = PropTypes.shape({
   uid: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  date: PropTypes.any.isRequired,
   address: PropTypes.string.isRequired,
   city: PropTypes.string.isRequired,
   state: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
+  acres: PropTypes.any.isRequired,
 });
 
 
---
+++ src/components/Pages/NewAppointmentForm/NewAppointmentForm.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './NewAppointmentForm.scss';
+// import appointmentRequests from '../../../Helpers/Data/Requests/appointmentRequests';
+import authRequests from '../../../Helpers/Data/authRequests';
+
+const defaultAppointment = {
+  firstName: '',
+  lastName: '',
+  date: '',
+  status: '',
+  address: '',
+  city: '',
+  state: '',
+  acres: '',
+  uid: '',
+};
+
+class NewAppointmentForm extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  }
+
+  state = {
+    newAppointment: defaultAppointment,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempAppointment = { ...this.state.newAppointment };
+    tempAppointment[name] = e.target.value;
+    this.setState({ newAppointment: tempAppointment });
+  }
+
+  appointmentChange = e => this.formFieldStringState('appointment', e);
+
+  dateChange = e => this.formFieldStringState('date', e);
+
+  firstNameChange = e => this.formFieldStringState('firstName', e);
+
+  lastNameChange = e => this.formFieldStringState('lastName', e);
+
+  addressChange = e => this.formFieldStringState('address', e);
+
+  cityChange = e => this.formFieldStringState('city', e);
+
+  stateChange = e => this.formFieldStringState('state', e);
+
+  acresChange = e => this.formFieldStringState('acres', e);
+
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const myAppointment = { ...this.state.newAppointment };
+    myAppointment.uid = authRequests.getCurrentUid();
+    onSubmit(myAppointment);
+    this.setState({ newAppointment: defaultAppointment });
+  }
+
+  // addAppointment = (newAppointment) => {
+  //   appointmentRequests.postRequest(newAppointment)
+  //     .then(() => {
+  //       appointmentRequests.getAllAppointments()
+  //         .then((appointments) => {
+  //           this.setState({ appointments });
+  //         });
+  //     })
+  //     .catch(err => console.error('error with appointments post', err));
+  // }
+
+  
+  render() {
+    const { newAppointment } = this.state;
+    return (
+      <div className="newAppointmentContainer mt-5">
+        <form onSubmit={this.formSubmit}>
+          <div className="newAppointmentForm">
+            <div className="newAppointmentHeader">Appointment Details</div>
+              <div className="topRow">
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="form-control"
+                  value={newAppointment.firstName}
+                  onChange={this.firstNameChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className="form-control"
+                  value={newAppointment.lastName}
+                  onChange={this.lastNameChange}
+                />
+              </div>
+              <div className="midRow">
+                <input
+                  type="text"
+                  placeholder="Address"
+                  className="form-control"
+                  value={newAppointment.address}
+                  onChange={this.addressChange}
+                />
+                <input 
+                  type="text"
+                  placeholder="City"
+                  className="form-control"
+                  value={newAppointment.city}
+                  onChange={this.cityChange}
+                />
+                <h3>Get Instant Quote!</h3>
+              </div>
+              <div className="bottomRow">
+                <input
+                  type="date"
+                  id="date"
+                  className="form-control"
+                  value={newAppointment.date}
+                  onChange={this.dateChange}
+                />
+                <input
+                  type="text"
+                  placeholder="State"
+                  className="form-control"
+                  value={newAppointment.state}
+                  onChange={this.stateChange}
+                />
+                <input
+                  type="number"
+                  placeholder="Acres"
+                  className="form-control"
+                  value={newAppointment.acres}
+                  onChange={this.acresChange}
+                />
+              </div>
+              <div className="commentHeader"><h3>Leave us a message!</h3></div>
+              <div className="commentAndPrice">
+              <textarea
+                className="commentInput"
+                placeholder="Comments/Message" 
+              />
+              <div className="">
+                <h1>$65</h1>
+              </div>
+              <div className="">
+              <p>is your quote</p>
+              </div>
+              </div>
+              <div className="makeAppointment">
+              <button className="btn btn-success">
+                Make Appointment
+              </button>
+              </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewAppointmentForm;
