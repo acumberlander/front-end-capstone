a34f7a413d7d195f620c97f56bcac8f21bdce17f
Austin Cumberlander <austincumberlander@gmail.com>
2019-05-01 19:35:32 -0500

    userHome links are all effed up..

--- src/components/UserHome/UserHome.js
+++ src/components/UserHome/UserHome.js
@@ -4,24 +4,35 @@ import './UserHome.scss';
 class UserHome extends React.Component {
   changeView = (e) => {
     const view = e.currentTarget.id;
-    this.props.history.push(`/${view}`);
+    if (view === 'newAppointment') {
+      this.props.history.push(`/${'newappointmentform'}`);
+    }
   }
 
   render() {
     return (
     <div className='Home mx-auto'>
       <div className="homeLinksContainer">
-        <div className="newAppointment">
+          <div id="newAppointmentText">
+            <h1>New Appointment</h1>
+          </div>
+        <div id="newAppointment"
+             onClick={this.changeView}>
           <img className="newAppointmentPic"
-               id="newappointmentform"
-               onClick={this.changeView}
-               src={require("../../img/cuttingGrass.jpeg")}
-               alt="New Appointment"></img>
+              id="newappointmentform"
+              src={require("../../img/cuttingGrass.jpeg")}
+              alt="New Appointment">
+          </img>
+          <div id="newAppointmentTextDiv"></div>
         </div>
-        <div   className="appointments">
+        <div id="appointmentText">
+            <h1>Appointments</h1>
+          </div>
+        <div  className="appointments"
+              onClick={this.changeView}>
+        <div id="appointmentTextDiv"></div>
           <img className="appointmentsPic"
                id="appointments"
-               onClick={this.changeView}
                src={require("../../img/manicuredGrass.jpeg")}
                alt="New Appointment">
           </img>
